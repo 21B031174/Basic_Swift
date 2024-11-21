f09a4b9d09e3b34a6267a0657518249edd5636bb
21B031174 <98838429+21B031174@users.noreply.github.com>
2024-11-22 01:09:16 +0500

    ass9

---
+++ Assignment_9/TabbedApp/TabbedAppUITests/TabbedAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TabbedAppUITestsLaunchTests.swift
+//  TabbedAppUITests
+//
+//  Created by Елшан Арсен on 15.11.2024.
+//
+
+import XCTest
+
+final class TabbedAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
